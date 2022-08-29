1581d14ad6c44cf0192c325a5ba38a401145a372
gunny6026 <gunny6026@naver.com>
2022-08-30 00:47:26 +0900

    feat : 변수 예제 코드

---
+++ ch04/4_10.js
@@ -0,0 +1,6 @@
+console.log(score); // undefined
+
+score = 80; // 값의 할당
+var score;
+
+console.log(score); // 80
---
+++ ch04/4_18.js
@@ -0,0 +1,13 @@
+// 카멜 케이스
+var firstName;
+
+// 스네이크 케이스
+var first_name;
+
+// 파스칼케이스
+var FirstName;
+
+// 헝가리언 케이스
+var strFirstName; // type + identifier
+var $elem = document.getElementById("myId"); // DOM 노드
+var observable$ = fromEvent(document , 'click'); // RxJS 옵저버블
