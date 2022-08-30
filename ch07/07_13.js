3dd20a4557869a164f6d87d48556aa535dcb9115
gunny6026 <gunny6026@naver.com>
2022-08-30 13:55:39 +0900

    docs : 동등/일치 비교 연산자

---
+++ ch07/07_13.js
@@ -0,0 +1,9 @@
+// 동등 비교, 결과를 예측하기 어렵다.
+
+console.log('0' == ''); // false
+console.log(0 == ''); // true
+console.log(0 == '0'); // true
+console.log(false == 'false'); // false
+console.log(false == '0'); // true
+console.log(false == null); // false
+console.log(false == undefined); // false
